676355156798beb6d924d42fdc844c25054b513a
anami17 <minorca.annamae17@gmail.com>
2023-08-30 17:35:52 +0800

    crudapp

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layout')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Icon</title>
-  </head>
-  <body>
-    <div class ="container mt-5">
+@section ('content')
+<div class ="container mt-5">
     <div>
         @if(session()->has('message'))
         <div class="alert alert-warning alert-dismissible fade show" role="alert">  
@@ -69,7 +59,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
